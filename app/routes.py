d4e6513a0f59fca7065ddf674597efee35d5a110
dschaly2 <drewschaly@gmail.com>
2022-12-18 14:55:15 -0500

    Added sample posts and base html template to include nav bar

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
+from flask import render_template
 from app import app
+from datetime import date
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World :)"
+  user = {'username': 'Boo'}
+
+  posts = [
+    { 'author': {'username': 'John'},
+      'body': 'Go Raiders!!',
+      'day': date.today()},
+    { 'author': {'username': 'Drew'},
+      'body': 'Big win by the brownies!',
+      'day': date.today() }
+  ]
+
+  return render_template('index.html', title="Home", user=user, posts=posts)
 
 
